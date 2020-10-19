d0a94eb5900882c4c1fd4e255eb3745e99b06109
Shellsea <elainesbarrera@gmail.com>
2020-10-19 14:14:35 -0700

    event list to begin quiz button, starts timer

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// target the button that begins quiz
+let beginBtn = document.querySelector("#begin");
+
+// when the button is clicked it runs the function countdown() which starts the timer
+beginBtn.addEventListener("click", function () {
+    console.log("begin quiz");
+    countdown();
+});
+
+
+
+
+
 
 // this targets the h3 which will eventually display a timer
 let timer = document.querySelector("#timer");
@@ -22,11 +35,17 @@ function countdown() {
 // called the function
 // countdown();
 
+//  when displaying score might need to use <span> to affect only the score and not the entire element
+ 
 
-// create a main page which when clicks starts, begins timer and asks questions
-// save each answer score to local storage and later on get it to display as highscore
-// create option buttons
-// create questions and then display if they were wrong or right 
-
-
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
 
