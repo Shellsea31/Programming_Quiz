a3859aee9e36e73377b9870d2b4d45bea632ce43
Shellsea <elainesbarrera@gmail.com>
2020-10-19 19:24:20 -0700

    dynamically changed text, added sub btn w/ boots properties

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-// target the button that begins quiz
-let beginBtn = document.querySelector("#begin");
-
-// when the button is clicked it runs the function countdown() which starts the timer
-beginBtn.addEventListener("click", function () {
-    countdown();
-    quiz();
-});
-
-
-
-
-
 
 // this targets the h3 which will eventually display a timer
 let timer = document.querySelector("#timer");
 
 // timer begins at 60 seconds
-let count = 60
+let count = 61
+// score begins at 0
+let score = 0
 
 // this function when called will start a timer at 60 counting down every second
 function countdown() {
@@ -25,23 +14,52 @@ function countdown() {
         count--
         // this 
         timer.textContent = (`Timer: ${count}s`);
-        if (count === 50) {
+        if (count === 55) {
             clearInterval(time)
         }
     }, 1000) //1,000 runs it every second
 }
 
-function quiz() {
-    let questions = document.querySelector("#text1")
-console.log(questions);
 
-questions.textContent = "Question 1";
+function quest1() {
+    // change the look of the area
+    let jumbo = document.querySelector(".jumbotron");
+    jumbo.setAttribute("class", "jumbotron-new");
+
+    // target the id and change the content
+    let number1 = document.querySelector("#text1");
+    number1.textContent = "Question 1";
+
+    // target the id and change the content
+    let question = document.querySelector("#text2");
+    question.textContent = "Commonly used data types DO NOT include:"
+
+    // create button and give it bootstrap style
+    let option1 = document.createElement("button");
+    option1.textContent = "1. Strings";
+    option1.setAttribute("type", "submit")
+    option1.setAttribute("class", "btn-sm btn-info")
+    console.log(option1);
+    jumbo.appendChild(option1);
+
 }
+
+// target the button that begins quiz
+let beginBtn = document.querySelector("#begin");
+
+// when the button is clicked it runs the function countdown() which starts the timer
+beginBtn.addEventListener("click", function () {
+    countdown();
+    quest1();
+});
 
 
 //  when displaying score might need to use <span> to affect only the score and not the entire element
 //  create a function for questions with if and else statements and call it at the click function
 // refer to WebApi activity 22 Local Storage Uh oh
+// use remove() - searched the web :)
+// you might need to add values to each question and push them to an array
+// then add the items in the array to get the high score.
 
 
 
